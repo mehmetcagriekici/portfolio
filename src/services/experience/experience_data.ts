c995dc95033a8ece6eeefcbbba37556c1f0f0ae0
unknown <mehmetcagriekici@gmail.com>
2025-01-02 10:49:42 +0300

    v0 final

--- src/services/experience/experience_data.ts
+++ src/services/experience/experience_data.ts
@@ -65,10 +65,10 @@ export const experience_data: ExperienceData = {
       date: "November 2024",
       technologies: ["React", "Tailwind", "Redux"],
       images: [
-        "large_screen__click_paint--login_form.jpg",
-        "large_screen__click_paint--map.jpg",
-        "small_screen__click_paint--login_form.jpg",
-        "small_screen__click_paint--map.jpg",
+        "../images/large_screen__click_paint--login_form.jpg",
+        "../images/large_screen__click_paint--map.jpg",
+        "../images/small_screen__click_paint--login_form.jpg",
+        "../images/small_screen__click_paint--map.jpg",
       ],
       summary:
         "You've ever played that game when you were a child, with your friends, around a table, with a notebook, a pen and a book, you'd open the book, and check the page number, and try to conquer the squared notebook page with the last digit of the page number? clickPaint is a demonstration of that child game with a dice. You can play it with up to 3 more players. Roll the dice, click on the squares and annoy your friends! Don't forget the best defense is a good offense.",
@@ -83,11 +83,11 @@ export const experience_data: ExperienceData = {
       date: "September 2024",
       technologies: ["React", "Styled Components"],
       images: [
-        "large_screen__safe_gossip--login_form.jpg",
-        "large_screen__safe_gossip--gossip_entry_form.jpg",
-        "large_screen__safe_gossip--summary.jpg",
-        "small_screen__safe_gossip--gossip_entry_form.jpg",
-        "small_screen__safe_gossip--summary.jpg",
+        "../images/large_screen__safe_gossip--login_form.jpg",
+        "../images/large_screen__safe_gossip--gossip_entry_form.jpg",
+        "../images/large_screen__safe_gossip--summary.jpg",
+        "../images/small_screen__safe_gossip--gossip_entry_form.jpg",
+        "../images/small_screen__safe_gossip--summary.jpg",
       ],
       summary:
         "We love to judge people, we love to gossip, and critisize our friends behind their backs. Now imagine and application in which you can make comments about your friends behaviors, appearances and intellects, and acutally rate them with your friends. And then, after reaching a certain amount of gossip, you can see a summary with a graph and and an average of what a person is. Create a key, share it with your other friends, and together eat your other other friend alive, and save the results for certain joy.",
@@ -135,11 +135,11 @@ export const experience_data: ExperienceData = {
         "React Popper",
       ],
       images: [
-        "all_screens__dark__rotjournal--login_form.jpg",
-        "all_screens__dark__rotjournal--preview.jpg",
-        "all_screens__light__rotjournal--login_form.jpg",
-        "all_screens__light__rotjournal--preview.jpg",
-        "all_screens__light__rotjournal--decrypted.jpg",
+        "../images/all_screens__dark__rotjournal--login_form.jpg",
+        "../images/all_screens__dark__rotjournal--preview.jpg",
+        "../images/all_screens__light__rotjournal--login_form.jpg",
+        "../images/all_screens__light__rotjournal--preview.jpg",
+        "../images/all_screens__light__rotjournal--decrypted.jpg",
       ],
       summary:
         "If you've ever needed a secret diary, this website will offer you one better. A secret and an encrypted journal. In this app, your entries will be converted to binary numbers after they are encrypted with a certain encryption method. ",
@@ -154,9 +154,9 @@ export const experience_data: ExperienceData = {
       date: "Februray 2024",
       technologies: ["React", "Styled Compoenents", "React Router DOM"],
       images: [
-        "all_screens__norskapp.jpg",
-        "small_screens__norskapp.jpg",
-        "large_screens__norskapp.jpg",
+        "../images/all_screens__norskapp.jpg",
+        "../images/small_screens__norskapp.jpg",
+        "../images/large_screens__norskapp.jpg",
       ],
       summary:
         "My first React project, I wanted to build an app to introduce Norway, but I didn't build the data. It's a powerpoint website, awating data to be created and included.",
--- src/services/personal/personal_data.ts
+++ src/services/personal/personal_data.ts
@@ -40,7 +40,7 @@ export const personal_data: PersonalData = {
       "coding",
       "binge watching tv series and watching movies",
     ],
-    photo: "pp.jpg",
+    photo: "../images/pp.jpg",
     socials: [
       { key: "gmail", value: "mehmetcagriekici@gmail.com" },
       {
