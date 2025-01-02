e3286c571ebcd5b89f80ecfc9e1f63042ffeb1d4
unknown <mehmetcagriekici@gmail.com>
2025-01-02 14:06:05 +0300

    v0 final

--- src/components/DetailsList.tsx
+++ src/components/DetailsList.tsx
@@ -23,30 +23,33 @@ const icons: {
 function DetailsList({ itemsArray }: DetailsListProps) {
   return (
     <ul className="h-full w-4/5 flex flex-col justify-safe-center items-safe-center gap-2 md:text-xl lg:text-2xl">
-      {itemsArray.map((item, i) => (
-        <li
-          key={i}
-          className={`w-full flex flex-col justify-safe-center items-safe-center gap-2 p-2 shadow-sm`}
-        >
-          {item.link && item.value ? (
-            <span className="flex gap-2 justify-center items-center">
-              {icons[item.key].icon}
-              <a
-                href={item.value}
-                target="blank"
-                className={`text-indigo-50 font-black tracking-wider capitalize`}
-              >
-                click here to visit {item.key}
-              </a>
-            </span>
-          ) : (
-            <span className="capitalize font-black tracking-wide text-indigo-100 flex justify-center items-center gap-2">
-              <span className="hidden md:flex">{item.key}</span>
-              {item.value}
-            </span>
-          )}
-        </li>
-      ))}
+      {itemsArray.map(
+        (item, i) =>
+          item.value && (
+            <li
+              key={i}
+              className={`w-full flex flex-col justify-safe-center items-safe-center gap-2 p-2 shadow-sm`}
+            >
+              {item.link ? (
+                <span className="flex gap-2 justify-center items-center">
+                  {icons[item.key].icon}
+                  <a
+                    href={item.value}
+                    target="blank"
+                    className={`text-indigo-50 font-black tracking-wider capitalize`}
+                  >
+                    click here to visit {item.key}
+                  </a>
+                </span>
+              ) : (
+                <span className="capitalize font-black tracking-wide text-indigo-100 flex justify-center items-center gap-2">
+                  <span className="hidden md:flex">{item.key}</span>
+                  {item.value}
+                </span>
+              )}
+            </li>
+          )
+      )}
     </ul>
   );
 }
